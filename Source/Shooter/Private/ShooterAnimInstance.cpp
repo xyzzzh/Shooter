0ea5a83efec3f17651a1c85374fbb8d3d6330522
xyz <972395684@qq.com>
2023-09-19 17:39:25 +0800

    feat: 添加ShooterAnimInstance类

---
+++ Source/Shooter/Private/ShooterAnimInstance.cpp
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ShooterAnimInstance.h"
+#include "ShooterCharacter.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+void UShooterAnimInstance::UpdateAnimationProperties(float DeltaTime)
+{
+	if (ShooterCharacter == nullptr) {
+		ShooterCharacter = Cast<AShooterCharacter>(TryGetPawnOwner());
+	}
+	if (ShooterCharacter) {
+		FVector Velocity{ ShooterCharacter->GetVelocity() };
+		Velocity.Z = 0.0f;
+		Speed = Velocity.Size();
+
+		bIsInAir = ShooterCharacter->GetCharacterMovement()->IsFalling();
+
+		if (ShooterCharacter->GetCharacterMovement()->GetCurrentAcceleration().Size() > 0.0f) {
+			bIsAccerlerating = true;
+		}
+		else {
+			bIsAccerlerating = false;
+		}
+	}
+}
+
+void UShooterAnimInstance::NativeInitializeAnimation()
+{
+	ShooterCharacter = Cast<AShooterCharacter>(TryGetPawnOwner());
+}
