2b0acbaa6e461ed88f3f073ab5cd5c1066fc5e4a
xyz <972395684@qq.com>
2023-11-17 13:52:15 +0800

    feat: zoom FOV when press mouse right key

--- Source/Shooter/Private/ShooterCharacter.cpp
+++ Source/Shooter/Private/ShooterCharacter.cpp
@@ -15,7 +15,10 @@
 // Sets default values
 AShooterCharacter::AShooterCharacter():
 	BaseTurnRate(45.f),
-	BaseLookUpRate(45.f)
+	BaseLookUpRate(45.f),
+	bAiming(false),
+	CameraDefaultFOV(0.f),	// set in BeginPlay()
+	CameraZoomFOV(60.f)
 {
 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
@@ -45,6 +48,11 @@ AShooterCharacter::AShooterCharacter():
 void AShooterCharacter::BeginPlay()
 {
 	Super::BeginPlay();
+
+	if (FollowCamera) {
+		CameraDefaultFOV = GetFollowCamera()->FieldOfView;
+
+	}
 }
 
 void AShooterCharacter::MoveForward(float Value)
@@ -191,6 +199,18 @@ bool AShooterCharacter::GetBeamEndLocation(
 	return false;
 }
 
+void AShooterCharacter::AimingButtonPressed()
+{
+	bAiming = true;
+	GetFollowCamera()->SetFieldOfView(CameraZoomFOV);
+}
+
+void AShooterCharacter::AimingButtonReleased()
+{
+	bAiming = false;
+	GetFollowCamera()->SetFieldOfView(CameraDefaultFOV);
+}
+
 // Called every frame
 void AShooterCharacter::Tick(float DeltaTime)
 {
@@ -215,4 +235,7 @@ void AShooterCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCo
 	PlayerInputComponent->BindAction("Jump", IE_Released, this, &ACharacter::StopJumping);
 
 	PlayerInputComponent->BindAction("FireButton", IE_Pressed, this, &AShooterCharacter::FireWeapon);
+
+	PlayerInputComponent->BindAction("AimingButton", IE_Pressed, this, &AShooterCharacter::AimingButtonPressed);
+	PlayerInputComponent->BindAction("AimingButton", IE_Released, this, &AShooterCharacter::AimingButtonReleased);
 }
--- Source/Shooter/Public/ShooterCharacter.h
+++ Source/Shooter/Public/ShooterCharacter.h
@@ -42,6 +42,10 @@ protected:
 
 	bool GetBeamEndLocation(const FVector& MuzzleSocketLocation, FVector& OutBeamLocation);
 
+	//set bAiming to true or false with button press
+	void AimingButtonPressed();
+	void AimingButtonReleased();
+
 public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
@@ -69,16 +73,26 @@ private:
 	UParticleSystem* MuzzleFlash;
 
 	/* Montage for firing weapon */
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Combat, meta = (AllowPrivateAccess = true))
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Combat, meta = (AllowPrivateAccess = true))
 	UAnimMontage* HipFireMontage;
 
 	/* Particles spawned upon bullet impact */
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Combat, meta = (AllowPrivateAccess = true))
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Combat, meta = (AllowPrivateAccess = true))
 	UParticleSystem* ImpactParticles;
 
-	/* Smoke trail for bullets*/
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Combat, meta = (AllowPrivateAccess = true))
+	/* Smoke trail for bullets */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Combat, meta = (AllowPrivateAccess = true))
 	UParticleSystem* BeamParticles;
+
+	//True when aiming
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Combat, meta = (AllowPrivateAccess = true))
+	bool bAiming;
+
+	//Default camera field of view value
+	float CameraDefaultFOV;
+
+	//Field of view value for when zoom in
+	float CameraZoomFOV;
 
 public:
 	FORCEINLINE USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
