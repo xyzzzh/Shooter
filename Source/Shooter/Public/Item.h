cce30b32707c94a11fe2210c99d7991593571381
xyz <972395684@qq.com>
2023-11-22 15:33:56 +0800

    feat: bind item name

--- Source/Shooter/Public/Item.h
+++ Source/Shooter/Public/Item.h
@@ -56,7 +56,10 @@ private:
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Item Properties", meta = (AllowPrivateAccess = "true"))
 	class USphereComponent* AreaSphere;
-
+	
+	// name which appears on the pickup widget
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Item Properties", meta = (AllowPrivateAccess = "true"))
+	FString ItemName;
 public:
 	FORCEINLINE UWidgetComponent* GetPickupWidget() const { return PickupWidget; }
 };
