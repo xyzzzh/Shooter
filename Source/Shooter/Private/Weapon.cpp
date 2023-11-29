6232dafaccc3d3d1a4e8fa80820cdb3d70004ed0
xyz <972395684@qq.com>
2023-11-29 13:25:57 +0800

    feat: weapon type

--- Source/Shooter/Private/Weapon.cpp
+++ Source/Shooter/Private/Weapon.cpp
@@ -2,10 +2,12 @@
 
 
 #include "Weapon.h"
+
 AWeapon::AWeapon() :
 	ThrowWeaponTime(0.7f),
 	bFalling(false),
-	Ammo(0)
+	Ammo(0),
+	WeaponType(EWeaponType::EWT_SubmachineGun)
 {
 	PrimaryActorTick.bCanEverTick = true;
 }
@@ -17,22 +19,22 @@ void AWeapon::Tick(float DeltaTime)
 	// Keep the Weapon upright
 	if (GetItemState() == EItemState::EIS_Falling && bFalling)
 	{
-		const FRotator MeshRotation{ 0.f, GetItemMesh()->GetComponentRotation().Yaw, 0.f };
+		const FRotator MeshRotation{0.f, GetItemMesh()->GetComponentRotation().Yaw, 0.f};
 		GetItemMesh()->SetWorldRotation(MeshRotation, false, nullptr, ETeleportType::TeleportPhysics);
 	}
 }
 
 void AWeapon::ThrowWeapon()
 {
-	FRotator MeshRotation{ 0.f, GetItemMesh()->GetComponentRotation().Yaw, 0.f };
+	FRotator MeshRotation{0.f, GetItemMesh()->GetComponentRotation().Yaw, 0.f};
 	GetItemMesh()->SetWorldRotation(MeshRotation, false, nullptr, ETeleportType::TeleportPhysics);
 
-	const FVector MeshForward{ GetItemMesh()->GetForwardVector() };
-	const FVector MeshRight{ GetItemMesh()->GetRightVector() };
+	const FVector MeshForward{GetItemMesh()->GetForwardVector()};
+	const FVector MeshRight{GetItemMesh()->GetRightVector()};
 	// Direction in which we throw the Weapon
 	FVector ImpulseDirection = MeshRight.RotateAngleAxis(-20.f, MeshForward);
 
-	float RandomRotation{ 30.f };
+	float RandomRotation{30.f};
 	ImpulseDirection = ImpulseDirection.RotateAngleAxis(RandomRotation, FVector(0.f, 0.f, 1.f));
 	ImpulseDirection *= 20'000.f;
 	GetItemMesh()->AddImpulse(ImpulseDirection);
@@ -43,15 +45,16 @@ void AWeapon::ThrowWeapon()
 		this,
 		&AWeapon::StopFalling,
 		ThrowWeaponTime);
-
 }
 
 void AWeapon::DecrementAmmo()
 {
-	if (Ammo < 1) {
+	if (Ammo < 1)
+	{
 		Ammo = 0;
 	}
-	else {
+	else
+	{
 		--Ammo;
 	}
 }
--- Source/Shooter/Public/Weapon.h
+++ Source/Shooter/Public/Weapon.h
@@ -6,6 +6,15 @@
 #include "Item.h"
 #include "Weapon.generated.h"
 
+UENUM(BlueprintType)
+enum class EWeaponType: uint8
+{
+	EWT_SubmachineGun UMETA(DisplayName = "Submachine Gun"),
+	EWT_AssaultRifle UMETA(DisplayName = "Assault Rifle"),
+
+	EWT_MAX UMETA(DisplayName = "DefaultMAX")
+};
+
 /**
  * 
  */
@@ -13,12 +22,15 @@ UCLASS()
 class SHOOTER_API AWeapon : public AItem
 {
 	GENERATED_BODY()
+
 public:
 	AWeapon();
 
 	virtual void Tick(float DeltaTime) override;
+
 protected:
 	void StopFalling();
+
 private:
 	FTimerHandle ThrowWeaponTimer;
 	float ThrowWeaponTime;
@@ -28,13 +40,18 @@ private:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapon Properties", meta = (AllowPrivateAccess = "true"))
 	int32 Ammo;
 
+	// the type of weapon
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapon Properties", meta = (AllowPrivateAccess = "true"))
+	EWeaponType WeaponType;
 
 public:
 	/** Adds an impulse to the Weapon */
 	void ThrowWeapon();
 
-	FORCEINLINE int32 GetAmmo()  const { return Ammo; }
+	FORCEINLINE int32 GetAmmo() const { return Ammo; }
 
 	//called from Character Class when fire weapon
 	void DecrementAmmo();
+
+	FORCEINLINE EWeaponType GetWeaponType() const { return WeaponType; }
 };
