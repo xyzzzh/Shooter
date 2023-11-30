dc13d96b17b14a38f78e657fe63b08931f4eb993
xyz <972395684@qq.com>
2023-11-30 15:58:06 +0800

    feat: rotate root bone

--- Source/Shooter/Private/ShooterAnimInstance.cpp
+++ Source/Shooter/Private/ShooterAnimInstance.cpp
@@ -6,22 +6,39 @@
 #include "GameFramework/CharacterMovementComponent.h"
 #include "Kismet/KismetMathLibrary.h"
 
+UShooterAnimInstance::UShooterAnimInstance():
+	Speed(0.f),
+	bIsInAir(false),
+	bIsAccerlerating(false),
+	MovementOffsetYaw(0.f),
+	LastMovementOffsetYaw(0.f),
+	bAiming(false),
+	CharacterYaw(0.f),
+	CharacterYawLastFrame(0.f),
+	RootYawOffset(0.f)
+{
+}
+
 void UShooterAnimInstance::UpdateAnimationProperties(float DeltaTime)
 {
-	if (ShooterCharacter == nullptr) {
+	if (ShooterCharacter == nullptr)
+	{
 		ShooterCharacter = Cast<AShooterCharacter>(TryGetPawnOwner());
 	}
-	if (ShooterCharacter) {
-		FVector Velocity{ ShooterCharacter->GetVelocity() };
+	if (ShooterCharacter)
+	{
+		FVector Velocity{ShooterCharacter->GetVelocity()};
 		Velocity.Z = 0.0f;
 		Speed = Velocity.Size();
 
 		bIsInAir = ShooterCharacter->GetCharacterMovement()->IsFalling();
 
-		if (ShooterCharacter->GetCharacterMovement()->GetCurrentAcceleration().Size() > 0.0f) {
+		if (ShooterCharacter->GetCharacterMovement()->GetCurrentAcceleration().Size() > 0.0f)
+		{
 			bIsAccerlerating = true;
 		}
-		else {
+		else
+		{
 			bIsAccerlerating = false;
 		}
 
@@ -29,15 +46,41 @@ void UShooterAnimInstance::UpdateAnimationProperties(float DeltaTime)
 		FRotator MovementRotation = UKismetMathLibrary::MakeRotFromX(ShooterCharacter->GetVelocity());
 		MovementOffsetYaw = UKismetMathLibrary::NormalizedDeltaRotator(MovementRotation, AimRotation).Yaw;
 
-		if (ShooterCharacter->GetVelocity().Size() > 0.f) {
+		if (ShooterCharacter->GetVelocity().Size() > 0.f)
+		{
 			LastMovementOffsetYaw = MovementOffsetYaw;
 		}
 
 		bAiming = ShooterCharacter->GetAiming();
 	}
+	TurnInPlace();
 }
 
 void UShooterAnimInstance::NativeInitializeAnimation()
 {
 	ShooterCharacter = Cast<AShooterCharacter>(TryGetPawnOwner());
+}
+
+void UShooterAnimInstance::TurnInPlace()
+{
+	if (ShooterCharacter == nullptr) return;
+	if (Speed > 0)
+	{
+		//don't want to turn in place; character is moving
+	}
+	else
+	{
+		CharacterYawLastFrame = CharacterYaw;
+		CharacterYaw = ShooterCharacter->GetActorRotation().Yaw;
+		const float YawDelta{CharacterYaw - CharacterYawLastFrame};
+
+		RootYawOffset -= YawDelta;
+		if (GEngine)
+		{
+			GEngine->AddOnScreenDebugMessage(1, -1, FColor::Blue,
+			                                 FString::Printf(TEXT("CharacterYaw: %f"), CharacterYaw));
+			GEngine->AddOnScreenDebugMessage(2, -1, FColor::Red,
+			                                 FString::Printf(TEXT("RootYawOffset: %f"), RootYawOffset));
+		}
+	}
 }
--- Source/Shooter/Public/ShooterAnimInstance.h
+++ Source/Shooter/Public/ShooterAnimInstance.h
@@ -13,33 +13,49 @@ UCLASS()
 class SHOOTER_API UShooterAnimInstance : public UAnimInstance
 {
 	GENERATED_BODY()
+
 public:
+	UShooterAnimInstance();
+	
 	UFUNCTION(BlueprintCallable)
 	void UpdateAnimationProperties(float DeltaTime);
 
 	virtual void NativeInitializeAnimation() override;
-	
+
+protected:
+	//handle turning in place vars
+	void TurnInPlace();
+
 private:
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = true))
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
 	AShooterCharacter* ShooterCharacter;
 
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = true))
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
 	float Speed;
 
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = true))
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
 	bool bIsInAir;
 
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = true))
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
 	bool bIsAccerlerating;
 
 	/* 用于扫射的Yaw偏移量 */
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Movement, meta = (AllowPrivateAccess = true))
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Movement, meta = (AllowPrivateAccess = "true"))
 	float MovementOffsetYaw;
 
 	/* 用于保存停止运动前最后的movement offset yaw */
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Movement, meta = (AllowPrivateAccess = true))
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Movement, meta = (AllowPrivateAccess = "true"))
 	float LastMovementOffsetYaw;
 
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = true))
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
 	bool bAiming;
+
+	// yaw of the character this frame
+	float CharacterYaw;
+
+	// yaw of the character the previous frame
+	float CharacterYawLastFrame;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Turn In Place", meta = (AllowPrivateAccess = "true"))
+	float RootYawOffset;
 };
