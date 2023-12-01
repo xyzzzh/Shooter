ae15126107744ac86b46a351d871249a1dfde8fb
xyz <972395684@qq.com>
2023-12-01 13:16:42 +0800

    feat: ammo class

---
+++ Source/Shooter/Private/Ammo.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Ammo.h"
+#include "Components/BoxComponent.h"
+#include "Components/WidgetComponent.h"
+#include "Components/SphereComponent.h"
+
+AAmmo::AAmmo()
+{
+	AmmoMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("AmmoMesh"));
+	SetRootComponent(AmmoMesh);
+
+	GetCollisionBox()->SetupAttachment(GetRootComponent());
+	GetPickupWidget()->SetupAttachment(GetRootComponent());
+	GetAreaSphere()->SetupAttachment(GetRootComponent());
+}
+
+void AAmmo::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+}
+
+void AAmmo::BeginPlay()
+{
+	Super::BeginPlay();
+}
